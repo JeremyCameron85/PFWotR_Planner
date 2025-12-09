83caed15147ae6f87e6292aa6d536a3bfa9a7ea1
JeremyCameron <131938234+JeremyCameron85@users.noreply.github.com>
2025-12-09 16:58:38 -0500

    Implemented logic to remove feats that no longer meet prereqs

--- ui/feats_tab.py
+++ ui/feats_tab.py
@@ -39,14 +39,28 @@ class FeatsTab(QWidget):
         available = self.character.available_feats(self.feats)
         
         if available:
-            self.feat_combo.addItems([feat["name"] for feat in available])
+            self.feat_combo.addItems([f["name"] for f in available])
         else:
             self.feat_combo.addItem("No feats available placeholder text")
 
     def add_selected_feat(self):
         selected_feat = self.feat_combo.currentText()
-        chosen_feat = next((feat for feat in self.feats if feat["name"] == selected_feat), None)
-        if chosen_feat and chosen_feat["name"] not in [feat["name"] for feat in self.character.feats]:
+        chosen_feat = next((f for f in self.feats if f["name"] == selected_feat), None)
+        if not chosen_feat:
+            return
+        
+        for prereq in chosen_feat.get("prerequisite_feats", []):
+            if prereq not in [f["name"] for f in self.character.feats]:
+                return
+            
+        for stat, value in chosen_feat.get("prerequisite_stats", {}).items():
+            if self.character.stats.get(stat, 0) < value:
+                return
+            
+        if self.character.level < chosen_feat.get("prerequisite_level", 1):
+            return
+        
+        if chosen_feat["name"] not in [f["name"] for f in self.character.feats]:
             self.character.feats.append(chosen_feat)
             self.update_feats()
             self.refresh_selected_feats()
@@ -56,12 +70,24 @@ class FeatsTab(QWidget):
         selected_items = self.selected_list.selectedItems()
         if not selected_items:
             return
+        
         feat_name = selected_items[0].text()
         self.character.feats = [f for f in self.character.feats if f["name"] != feat_name]
+        removed = {feat_name}
+        changed = True
+        while changed:
+            changed = False
+            for feat in list(self.character.feats):
+                prereqs = feat.get("prerequisite_feats",  [])
+                if any(req  in removed for req in prereqs):
+                    self.character.feats.remove(feat)
+                    removed.add(feat["name"])
+                    changed = True
+                    
         self.update_feats()
         self.refresh_selected_feats()
         self.feats_changed.emit()
-        
+
     def refresh_selected_feats(self):
         self.selected_list.clear()
-        self.selected_list.addItems([feat["name"] for feat in self.character.feats])
+        self.selected_list.addItems([f["name"] for f in self.character.feats])
