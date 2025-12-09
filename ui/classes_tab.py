c430e5618bf61b310245bead2d06db55ff45b407
JeremyCameron <131938234+JeremyCameron85@users.noreply.github.com>
2025-12-09 18:45:18 -0500

    First step in adding archetypes and class descriptions

--- ui/classes_tab.py
+++ ui/classes_tab.py
@@ -1,8 +1,6 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QComboBox
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QTreeWidget, QTreeWidgetItem, QTextEditor
 from PyQt6.QtCore import pyqtSignal
-from pathlib import Path
 from wotr_planner.models.json_loader import load_classes
-import json
 
 class ClassTab(QWidget):
     class_changed = pyqtSignal()
@@ -16,21 +14,47 @@ class ClassTab(QWidget):
         self.classes = load_classes()
 
         layout.addWidget(QLabel("Select Class:"))
-        self.class_combo = QComboBox()
-        self.class_combo.addItems([char_class["name"] for char_class in self.classes])
-        layout.addWidget(self.class_combo)
+        self.class_tree = QTreeWidget()
+        self.class_tree.setHeaderHidden(True)
+        layout.addWidget(self.class_tree)
 
-        if getattr(self.character, "char_class", None):
-            idx = next(
-                (i for i, c in enumerate(self.classes)
-                 if c["name"] == self.character.char_class.get("name")),
-                 0
+        self.description_box = QTextEditor()
+        self.description_box.setReadOnly(True)
+        layout.addWidget(self.description_box, stretch=1)
+
+        self.populate_classes()
+        self.class_tree.itemClicked.connect(self.on_item_selected)
+
+    def populate_classes(self):
+        self.class_tree.clear()
+        for cls in self.classes:
+            parent_item = QTreeWidgetItem([cls["name"]])
+            parent_item.setData(0, 1, cls)
+            self.class_tree.addTopLevelItem(parent_item)
+
+            for arch in cls.get("archetypes", []):
+                child_item = QTreeWidgetItem([arch["name"]])
+                child_item.setData(0, 1, arch)
+                parent_item.addChild(child_item)
+
+        self.class_tree.collapseAll()
+
+    def on_item_selected(self, item, column):
+        parent = item.parent()
+        if parent is None:
+            self.class_tree.collapseAll()
+            item.setExpanded(True)
+            cls = item.data(0, 1)
+            self.character.char_class = cls
+            self.character.archetype = None
+            self.description_box.setPlainText(
+                f"{cls['name']} (Hit Die: d{cls['hit_die']}, Skill Points: {cls['skill_points']})"
             )
-            self.class_combo.setCurrentIndex(idx)
+        else:
+            arch = item.data(0, 1)
+            base_cls = parent.data(0, 1)
+            self.character.char_class = base_cls
+            self.character.archetype = arch["name"]
+            self.description_box.setPlainText(arch.get("description", "No description available."))
 
-        self.class_combo.currentIndexChanged.connect(self.update_class)
-
-    def update_class(self, index):
-        selected_class = self.classes[index]
-        self.character.char_class = selected_class
         self.class_changed.emit()
